edc39fb9bcdaf26dd6c9b622b4b6e72176859360
LERICHE Sébastien <103203151+nalaknl@users.noreply.github.com>
2022-05-04 15:03:24 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,21 +3,7 @@ document
     .addEventListener("mousemove", (e) => {
         const x = e.pageX - e.target.offsetLeft;
         const y = e.pageY - e.target.offsetTop;
-        console.log(x + " / " + y);
+
         e.target.style.setProperty("--x", `${x}px`);
         e.target.style.setProperty("--y", `${y}px`);
     });
-// document.getElementById("bouton2").addEventListener("mousemove", (e) => {
-//     const x = e.pageX - e.target.offsetLeft;
-//     const y = e.pageY - e.target.offsetTop;
-//     console.log(x + " / " + y);
-//     e.target.style.setProperty("--x", `${x}px`);
-//     e.target.style.setProperty("--y", `${y}px`);
-// });
-// document.getElementById("bouton3").addEventListener("mousemove", (e) => {
-//     const x = e.pageX - e.target.offsetLeft;
-//     const y = e.pageY - e.target.offsetTop;
-//     console.log(x + " / " + y);
-//     e.target.style.setProperty("--x", `${x}px`);
-//     e.target.style.setProperty("--y", `${y}px`);
-// });
